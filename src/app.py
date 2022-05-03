228db492443d9f1aad822ab00a0b80b8a7cbf19c
gerrykou <gerasimoskounadis@gmail.com>
2022-05-03 21:12:58 +0100

    refactor, write to csv

--- src/app.py
+++ src/app.py
@@ -1,62 +1,103 @@
 import logging
 import xlrd
+import csv
+from itertools import zip_longest
 
+from typing import Any, Dict, List, Tuple
 
-def read_xls(xls_filepath: str) -> dict:
+expected_labels = {
+    'NET LOAD':(10, 1),
+    'TOTAL IMPORTS':(24, 1),
+    'TOTAL EXPORTS':(30, 1),
+    'EXPORTS-IMPORTS':(31,1),
+    'TOTAL LIGNITE':(51, 1),
+    'TOTAL GAS':(83, 1),
+    'TOTAL HYDRO':(106, 1),
+    'TOTAL RES':(187, 1)
+    } # TODO {{"label":'NET LOAD',"row":10,"col":1},{"label":'TOTAL IMPORTS',"row":24,"col":1}}
+
+def read_xls(xls_filepath: str, sheet_number: int) -> Dict:
     '''Read an xls file, return a dictionary with keys()=[sheet_name, content]'''
-    f = xlrd.open_workbook(xls_filepath)
-    logging.debug(f'Worksheet name(s): {f.sheet_names()}')
-    sheet_number = 1
-    sh = f.sheet_by_index(sheet_number) # selects the 2nd sheet
-    logging.debug(f'{sh.name}, {sh.nrows}, {sh.ncols}')
+    with xlrd.open_workbook(xls_filepath) as f:
+        logging.debug(f'Worksheet name(s): {f.sheet_names()}')
+        # f.sheet_by_name()
 
-    file_content = []
-    for rx in range(sh.nrows):
-        file_content.append(sh.row(rx))
-    # logging.debug(file_content)
+        sh = f.sheet_by_index(sheet_number)
+        logging.debug(f'{sh.name}, {sh.nrows}, {sh.ncols}')
 
-    output_dict = {"sheet_name":sh.name, "content":file_content}
-    # print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
+        file_content = []
+        for rx in range(sh.nrows):
+            row_content = []
+            file_content.append(row_content)
+            for cx in range(sh.ncols):
+                row_content.append(sh.cell_value(rowx=rx, colx=cx))
+
+        # logging.debug(file_content)
+        output_dict = {"sheet_name":sh.name, "content":file_content, "nrows":sh.nrows , "ncols":sh.ncols }
     return output_dict
 
-def test_content(data: list):
-    # TODO a known_labels dictionary {[row,col]:'known_label', [50,1]:'TOTAL LIGNITE'}
-    row = 51
-    col = 1
-    cell_value = _get_cell_value(row, col, data)
-    if cell_value == 'TOTAL LIGNITE':
-        print(cell_value)
-    else:
-        logging.error(f'TOTAL LIGNITE, not found in {data[row][col]}')
+def validate_content(data: list, expected_labels: Dict[str, Tuple]):
+    for item in expected_labels.items():
+        label = item[0]
+        row = item[1][0]
+        col = item[1][1]
+        # logging.debug(f'label:{label}, row:{row}, col:{col}')
+        cell_value = _get_cell_value(row, col, data)
+        if cell_value == label:
+            pass
+        else:
+            raise Exception(f'{label}, not found in row:{row}, col:{col}, {data[row][col]}')
+    return True
 
 def _get_cell_value(row: int, col: int, data: list):
-    cell = str(data[row][col])
-    cell_list = cell.split(':')
-    cell_value = cell_list[1].strip("'")
+    cell_value = data[row][col]
     return cell_value
-
-def parse_hours(data: list):
-    '''Return hours list'''
-    hours = []
-    for i in range(2,26):
-        cell_value = _get_cell_value(34, i, data)
-        hours.append(cell_value)
-    return hours
-
-def get_sum_lignite(data: list):
-    sum_lignite = _get_cell_value(51, 26, data)
-    logging.debug(f'sum_lignite:{sum_lignite}')
-    return sum_lignite
     #self.sum_lignite = _get_cell_value(51, 26, data) #take row,col from known_values dict {"sum_lignite":[row,col]}
 
+def parse_row_to_list(row: int , col_start: int, col_end: int, data: list) -> List:
+    output = []
+    for i in range(col_start,col_end):
+        cell_value = _get_cell_value(row, i, data)
+        output.append(cell_value)
+    return output
+
+# def export_to_csv(self, filename):
+#     with open(filename , mode="w") as csv_file:
+#         fieldnames = ["date_month", "posts_sum", "comments_sum"]
+#         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#         writer.writeheader()
+#         for date, sum in self.calculate_monthly_stats().items():
+#             writer.writerow({ "date_month" : date , "posts_sum": sum["posts_sum"], "comments_sum": sum["comments_sum"]})
+#     return None
+
+def write_to_csv(filepath: str, data_lists: List):
+    export_data = zip_longest(*data_lists, fillvalue = '')
+    with open(filepath , mode="w") as csv_file:
+        wr = csv.writer(csv_file)
+        wr.writerows(export_data)  
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
     xls_filename = '20220430_SystemRealizationSCADA_01.xls'
+    date = xls_filename[:8]
+
     folder = 'data'
     filepath = f'{folder}/{xls_filename}'
-    data_dict = read_xls(filepath)
-    test_content(data_dict['content'])
-    hours_list = parse_hours(data_dict['content'])
-    sum_lignite = get_sum_lignite(data_dict['content'])
+    sheet_number = 1 # 1 is the 2nd worksheet
+    data_dict = read_xls(filepath, sheet_number)
+    validate_content(data_dict['content'], expected_labels)
+
+    index_list = parse_row_to_list(row=4 , col_start=1, col_end=26, data=data_dict['content']) # col_end=27 if SUM want to be included
+    print(index_list)
+
+    lignite_row = parse_row_to_list(row=51 , col_start=1, col_end=26, data=data_dict['content'])
+    print(lignite_row)
+
+    gas_row = parse_row_to_list(row=83 , col_start=1, col_end=26, data=data_dict['content'])
+    res_row = parse_row_to_list(row=187 , col_start=1, col_end=26, data=data_dict['content'])
+
+    data_lists = (index_list, lignite_row, gas_row, res_row)
+    output_folder = 'data/output'
+    csv_filepath = f'{output_folder}/{date}.csv'
+    write_to_csv(csv_filepath, data_lists)
--- test/test_app.py
+++ test/test_app.py
@@ -5,15 +5,63 @@ import src.app as app
 # TEST FUNCTIONS
 def test_read_xls():
     my_file = 'test/test.xls'
-    actual_output = app.read_xls(my_file)
+    sheet_number = 1
+    actual_output = app.read_xls(my_file, sheet_number)
     expected_output = 'System_Production'
     assert actual_output['sheet_name'] == expected_output
     assert 'sheet_name' in actual_output.keys()
     assert 'content' in actual_output.keys()
+    assert 'nrows' in actual_output.keys()
+    assert 'ncols' in actual_output.keys()
 
 def test_get_cell_value():
     my_file = 'test/test.xls'
-    data = app.read_xls(my_file)['content']
-    actual_output = app._get_cell_value(51,1,data)
-    expected_output = 'TOTAL LIGNITE'
-    assert actual_output == expected_output
+    sheet_number = 1
+    data = app.read_xls(my_file, sheet_number)['content']
+    assert app._get_cell_value(10,1,data) == 'NET LOAD'
+    assert app._get_cell_value(51,1,data) == 'TOTAL LIGNITE'
+    assert app._get_cell_value(83,1,data) == 'TOTAL GAS'
+    assert app._get_cell_value(106,1,data) == 'TOTAL HYDRO'
+    assert app._get_cell_value(187,1,data) == 'TOTAL RES'
+
+def test_validate_content():
+    my_file = 'test/test.xls'
+    sheet_number = 1
+    data = app.read_xls(my_file, sheet_number)['content']
+    known_labels = {
+    'NET LOAD':(10, 1),
+    'TOTAL IMPORTS':(24, 1),
+    'TOTAL EXPORTS':(30, 1),
+    'EXPORTS-IMPORTS':(31,1),
+    'TOTAL LIGNITE':(51, 1),
+    'TOTAL GAS':(83, 1),
+    'TOTAL HYDRO':(106, 1),
+    'TOTAL RES':(187, 1)
+    }
+    assert app.validate_content(data, known_labels) == True
+
+def test_parse_row_to_list():
+    my_file = 'test/test.xls'
+    sheet_number = 1
+    data = app.read_xls(my_file, sheet_number)['content']
+    actual_output1 = app.parse_row_to_list(row=51 , col_start=1, col_end=26, data=data)
+    expected_output1 = [
+        'TOTAL LIGNITE', 204.0, 190.0, 191.0, 191.0, 191.0, 189.0, 190.0, 190.0, 189.0, 189.0, 187.0,
+        192.0, 199.0, 200.0, 190.0, 190.0, 191.0, 195.0, 195.0, 199.0, 203.0, 219.0, 229.0, 204.0
+        ]
+    actual_output2 = app.parse_row_to_list(row=4 , col_start=1, col_end=26, data=data)
+    expected_output2 = [
+        '', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', 
+        '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'
+        ]
+    assert actual_output1 == expected_output1
+    assert actual_output2 == expected_output2
+
+# TEST FAILURES
+def test_rvalidate_content_error():
+    my_file = 'test/test.xls'
+    sheet_number = 1
+    data = app.read_xls(my_file, sheet_number)['content']
+    with pytest.raises(Exception):
+        known_labels = {'wrong_value':(10, 1),}
+        app.validate_content(data, known_labels)
